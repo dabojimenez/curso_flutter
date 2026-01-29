451e43de07bf59adca1a874bb38a07e2a875ce78
dabojimenez <dabojimenez16@hotmail.com>
2026-01-29 11:43:44 -0500

    Estilo global para la aplicación

--- yes_no_app/lib/main.dart
+++ yes_no_app/lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
+      // theme: // Se refiere a todo A TODOO el tema en general
+      theme: AppTheme(selectedColor: 2).theme(),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Material App Bar'),
         ),
         body: Center(
-          child: FilledButton(
+          child: FilledButton.tonal(
             onPressed: () {},
             child: const Text('Click me!!')
           ),
