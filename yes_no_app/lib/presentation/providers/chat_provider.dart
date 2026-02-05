e2061cb3790ccbc5f4fe864ca2da1017f2316c9f
dabojimenez <dabojimenez16@hotmail.com>
2026-02-05 14:13:05 -0500

    Provider - Gestor de estado

---
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+// ChangeNotifier: Puedenotificar cunado existen cambios
+class ChatProvider extends ChangeNotifier {
+  List<Message> messages = [
+    Message(text: 'Hola', fromWho: FromWho.me),
+    Message(text: 'Como estas?', fromWho: FromWho.me),
+  ];
+
+  Future<void> sendMessage(String text) async{
+    // TODO: implementar metodo
+  }
+}
