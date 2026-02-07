d3c9e78d268e3365e10a820e31140c2f8e7e5f83
dabojimenez <dabojimenez16@hotmail.com>
2026-02-06 20:13:52 -0500

    Instalar Provider

--- yes_no_app/lib/main.dart
+++ yes_no_app/lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/screens/chat/chat_screen.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Yes No App',
-      debugShowCheckedModeBanner: false,
-      // theme: // Se refiere a todo A TODOO el tema en general
-      theme: AppTheme(selectedColor: 2).theme(),
-      home: const ChatScreen(),
+    // MultiProvider: para proeer varios datos, ser proveidos en la aplicacion
+    return MultiProvider(
+      providers: [
+        // _ : como no vamos a usar el context, le decimos que no usaremos ese argumetno
+        ChangeNotifierProvider(create: (_) => ChatProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Yes No App',
+        debugShowCheckedModeBanner: false,
+        // theme: // Se refiere a todo A TODOO el tema en general
+        theme: AppTheme(selectedColor: 2).theme(),
+        home: const ChatScreen(),
+      ),
     );
   }
 }
