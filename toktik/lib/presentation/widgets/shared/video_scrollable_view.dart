8e941e5351a19e83fe0843531a649bee541d60d0
dabojimenez <dabojimenez16@hotmail.com>
2026-02-10 21:15:08 -0500

    Botones de like y views

--- toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -9,15 +10,26 @@ class VideoScrollableView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // PageView: similar a un listado, nos pemrite un scrol,a pantalla completa
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical, // Habilitamos el scroll vertical
       // Habilitamos para android, para que el scroll sea rebotante
       physics: const BouncingScrollPhysics(),
-      children: [
-        Container(color: Colors.red,),
-        Container(color: Colors.green,),
-        Container(color: Colors.blue,),
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            // Video Player + Gradiente
+            // Botones
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost),
+            ),
+          ],
+        );
+      },
     );
   }
 }
+
