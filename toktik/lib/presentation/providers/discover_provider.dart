4ccb2bc156272ad7b3063f20b8096c55955b5ea9
dabojimenez <dabojimenez16@hotmail.com>
2026-02-13 12:47:25 -0500

    Reproducir videos

--- toktik/lib/presentation/providers/discover_provider.dart
+++ toktik/lib/presentation/providers/discover_provider.dart
@@ -4,25 +4,20 @@ import 'package:toktik/infraestructure/models/local_video_model.dart';
 import 'package:toktik/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
-
   // TODO: Repository, DataSource
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-
-    await Future.delayed(const Duration(seconds: 2));
-
     final List<VideoPost> newVideos = videoPosts
-      .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
-      .toList();
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList();
 
     videos.addAll(newVideos);
 
     initialLoading = false;
-    
+
     notifyListeners();
   }
-
 }
--- toktik/lib/presentation/widgets/video/fullscreen_player.dart
+++ toktik/lib/presentation/widgets/video/fullscreen_player.dart
@@ -34,7 +34,28 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
     return FutureBuilder(
       future: _controller.initialize(),
       builder: (context, snapshot) {
-        return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+        // Aqui se muestra el CircularProgressIndicator, mientras se inicializa el video
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+        }
+
+        // Aqui se muestra el video
+        return AspectRatio(
+          aspectRatio: _controller.value.aspectRatio,
+          child: Stack(
+            children: [
+              VideoPlayer(_controller),
+              // Gradiente
+
+              // Texto
+              Positioned(
+                bottom: 50,
+                left: 20,
+                child: _VideoCaption(caption: widget.caption),
+              ),
+            ],
+          ),
+        );
       },
     );
   }
@@ -44,5 +65,22 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
     _controller.dispose();
 
     super.dispose();
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+  final String caption;
+
+  const _VideoCaption({Key? key, required this.caption}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Obtenemos el ancho de la pantalla
+    final size = MediaQuery.of(context).size;
+    final titleStile = Theme.of(context).textTheme.titleLarge;
+    return SizedBox(
+      width: size.width * 0.6, // El 60% del ancho de la pantalla
+      child: Text(caption, maxLines: 2, style: titleStile),
+    );
   }
 }
