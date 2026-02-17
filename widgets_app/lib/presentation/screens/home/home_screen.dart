2902e6ad8a3fb988c6054d29550967e2307c3e88
dabojimenez <dabojimenez16@hotmail.com>
2026-02-16 21:41:45 -0500

    ListView

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,6 +10,47 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView.builder(
+      // physics: const BouncingScrollPhysics(),
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItems = appMenuItems[index];
+        return _CustomListTitle(menuItems: menuItems);
+      },
+    );
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+  const _CustomListTitle({
+    required this.menuItems,
+  });
+
+  final MenuItems menuItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItems.icon, color: theme.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_sharp, color: theme.primary,),
+      title: Text(menuItems.title),
+      subtitle: Text(menuItems.subtitle),
+      onTap: () {
+        // TODO: Navegar a otra pantalla
+      },
     );
   }
 }
