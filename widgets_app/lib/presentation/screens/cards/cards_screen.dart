526e6cae5fa4f85cdc113545e5ac6278554c0267
dabojimenez <dabojimenez16@hotmail.com>
2026-02-18 22:03:01 -0500

    Tarjetas con borde

--- widgets_app/lib/presentation/screens/cards/cards_screen.dart
+++ widgets_app/lib/presentation/screens/cards/cards_screen.dart
@@ -32,10 +32,10 @@ class _CardsView extends StatelessWidget {
             (card) =>
                 _CardType1(label: card['label'], elevation: card['elevation']),
           ),
-          // ...cards.map(
-          //   (card) =>
-          //       _CardType1(label: card['label'], elevation: card['elevation']),
-          // ),
+          ...cards.map(
+            (card) =>
+                _CardType2(label: card['label'], elevation: card['elevation']),
+          ),
         ],
       ),
     );
@@ -54,19 +54,54 @@ class _CardType1 extends StatelessWidget {
       elevation: elevation,
       child: Padding(
         padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
-        child: Column(children: [
-          Align(
-            alignment: Alignment.topRight,
-            child: IconButton(
-              icon: const Icon(Icons.more_vert_outlined),
-              onPressed: () {},
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Text(label),
-          ),
-        ]),
+            Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({super.key, required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      // shape, es la forma basicamente
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: colors.outline,
+        width: 1.0),
+        borderRadius: BorderRadius.all(const Radius.circular(12)),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(alignment: Alignment.bottomLeft, child: Text('$label - ouline')),
+          ],
+        ),
       ),
     );
   }
