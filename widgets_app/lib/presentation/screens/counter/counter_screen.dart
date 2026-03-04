cc54e3450022228bcb73b5df8ae7e8e634526ed8
dabojimenez <dabojimenez16@hotmail.com>
2026-03-03 21:50:52 -0500

    Cambiar valores y modificadores

--- widgets_app/lib/presentation/screens/counter/counter_screen.dart
+++ widgets_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
@@ -12,10 +13,19 @@ class CounterScreen extends ConsumerWidget {
 
     // watch: que este pendiente del counterProvider (escucha cada vez que cambia)
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contador - Riverpod'),
+        actions: [
+          IconButton(
+            icon: Icon(isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkModeProvider.notifier).state = !isDarkMode;
+            },
+          )
+        ],
       ),
       body: Center(
         child: Text('Valor: $clickCounter'
@@ -24,7 +34,15 @@ class CounterScreen extends ConsumerWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          // PESIMA PRACTICA USAR EL WATCH, EN METODOS
+          // ref.watch(counterProvider);
+          // Siempre usar un read, para modificar el estado
+          // state: es el valor que se retorna
+          ref.read(counterProvider.notifier).state++;
+          // similar al read, de arriba
+          // ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
       ),
     );
   }
