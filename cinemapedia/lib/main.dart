db3153e69e40428af333ed09e36ff687fa9bab75
dabojimenez <dabojimenez16@hotmail.com>
2026-03-06 11:39:51 -0500

    proyecto inicial de cinemapedia

---
+++ cinemapedia/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme().getTheme(),
+      home: const Scaffold(body: Center(child: Text('Hello World!'))),
+    );
+  }
+}
