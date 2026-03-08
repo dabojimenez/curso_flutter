d79de6f0f3a87f9e8361f44e3a707707cb46d3a3
dabojimenez <dabojimenez16@hotmail.com>
2026-03-07 20:51:05 -0500

    Realizar el mapeo de la respuesta de MovieDB

--- cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
+++ cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasource/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class MovieDbDataSource extends MoviesDatasource {
@@ -17,8 +19,11 @@ class MovieDbDataSource extends MoviesDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    
-    final List<Movie> movies = [];
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDbResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
     return movies;
   }
 }
--- cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
+++ cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
@@ -15,7 +15,7 @@ class MovieMapper {
     popularity: moviedb.popularity,
     posterPath: (moviedb.posterPath != '')
         ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-        : 'https://th.bing.com/th/id/OIP.Lr_j_PgqTGzKxJTeIwajVwHaLH?w=204&h=306&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
+        : 'no-poster',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
