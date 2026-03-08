6669957c2814f66e481867221569fb3daa18a737
dabojimenez <dabojimenez16@hotmail.com>
2026-03-08 17:42:13 -0500

    MovieRepository - Implementación

---
+++ cinemapedia/lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:cinemapedia/domain/datasource/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+  // llamaos al datasource, llamando a la clase padre MoviesDatasource
+  final MoviesDatasource datasource;
+
+  MovieRepositoryImpl(this.datasource);
+  
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+  
+}
