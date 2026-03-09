a94294dc7f1a53e5b97905c41ab058771e272ecc
dabojimenez <dabojimenez16@hotmail.com>
2026-03-08 20:40:11 -0500

    Custom AppBar

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/screens/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -35,12 +36,19 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     if (nowPlayingMovies.isEmpty)
       return const Center(child: CircularProgressIndicator());
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(title: Text(movie.title));
-      },
+    return Column(
+      children: [
+        CustomAppbar(),
+        Expanded(
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context, index) {
+              final movie = nowPlayingMovies[index];
+              return ListTile(title: Text(movie.title));
+            },
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final titleStyle = Theme.of(context).textTheme.titleMedium;
+
+    return SafeArea(
+      bottom: false,
+      child: Container(
+        color: Colors.amber,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: SizedBox(
+            width: double.infinity, // todo el anchoq ue se pueda
+            child: Row(
+              children: [
+                Icon(Icons.movie_outlined, color: colors.primary),
+                const SizedBox(width: 5,),
+                Text('Cinemapedia', style: titleStyle,),
+                // Spacer: es como un espacio flexible que ocupa todo el espacio restante
+                Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
