507a3e729a96c80f5cadd526c3ad0813edda6807
dabojimenez <dabojimenez16@hotmail.com>
2026-03-11 14:37:24 -0500

    Mostrar las películas en el ListView

--- cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
@@ -23,6 +24,83 @@ class MovieHorizontalListview extends StatelessWidget {
         children: [
           if (title != null || subtitle != null)
             _Title(title: title, subtitle: subtitle),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: movies.length,
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return _Slide(movie: movies[index]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // alineamos al inicio
+        children: [
+          //* Imagen
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: const CircularProgressIndicator(strokeWidth: 2),
+                      ),
+                    );
+                  }
+                  return FadeIn(child: child);
+                },
+              ),
+            ),
+          ),
+          SizedBox(height: 5),
+          //* Titulo
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title,
+              maxLines: 2, // Permite la cantidad de lineas a mostrar
+              style: textStyles.titleSmall,
+            ),
+          ),
+
+          //* Rating
+          Row(
+            children: [
+              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+              const SizedBox(width: 10),
+              Text(
+                '${movie.voteAverage}',
+                style: textStyles.bodyMedium?.copyWith(
+                  color: Colors.yellow.shade800,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Text('${movie.popularity}', style: textStyles.bodySmall),
+            ],
+          ),
         ],
       ),
     );
