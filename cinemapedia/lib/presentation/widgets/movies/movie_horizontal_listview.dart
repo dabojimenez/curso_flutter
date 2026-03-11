ef4db0598275b07ca5f6e3e44f83397b615f73b1
dabojimenez <dabojimenez16@hotmail.com>
2026-03-11 14:59:06 -0500

    HumanFormats - Números cortos

--- cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
@@ -87,19 +88,25 @@ class _Slide extends StatelessWidget {
           ),
 
           //* Rating
-          Row(
-            children: [
-              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
-              const SizedBox(width: 10),
-              Text(
-                '${movie.voteAverage}',
-                style: textStyles.bodyMedium?.copyWith(
-                  color: Colors.yellow.shade800,
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+                const SizedBox(width: 10),
+                Text(
+                  '${movie.voteAverage}',
+                  style: textStyles.bodyMedium?.copyWith(
+                    color: Colors.yellow.shade800,
+                  ),
                 ),
-              ),
-              const SizedBox(width: 10),
-              Text('${movie.popularity}', style: textStyles.bodySmall),
-            ],
+                Spacer(),
+                Text(
+                  HumanFormats.number(movie.popularity.toDouble()),
+                  style: textStyles.bodySmall,
+                ),
+              ],
+            ),
           ),
         ],
       ),
