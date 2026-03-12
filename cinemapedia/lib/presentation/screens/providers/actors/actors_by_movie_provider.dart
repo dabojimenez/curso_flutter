b7823c6efcd6737a515cdb27d20ff6c245c515bf
dabojimenez <dabojimenez16@hotmail.com>
2026-03-12 16:07:31 -0500

    Repositorio y Provider - Implementación

---
+++ cinemapedia/lib/presentation/screens/providers/actors/actors_by_movie_provider.dart
@@ -0,0 +1,36 @@
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/presentation/screens/providers/actors/actors_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final actorsByMovieProvider =
+    StateNotifierProvider<ActorsByMovieNotifier, Map<String, List<Actor>>>((
+      ref,
+    ) {
+      final actorRepository = ref.watch(actorsRepositoryProvider);
+      return ActorsByMovieNotifier(getActors: actorRepository.getActorsByMovie);
+    });
+
+/*
+{
+  '548': List<Actor>(),
+  '578': List<Actor>(),
+  '518': List<Actor>(),
+}
+*/
+
+typedef GetActorsCallback = Future<List<Actor>> Function(String movieId);
+
+class ActorsByMovieNotifier extends StateNotifier<Map<String, List<Actor>>> {
+  final GetActorsCallback getActors;
+
+  ActorsByMovieNotifier({required this.getActors}) : super({});
+
+  Future<void> loadActors(String movieId) async {
+    // no regresmaos nada, si ya consultamos anteriromente (cache interno de la aplicacion)
+    if (state[movieId] != null) return;
+
+    final List<Actor> actors = await getActors(movieId);
+
+    state = {...state, movieId: actors};
+  }
+}
