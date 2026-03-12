f5450f9b592b98bc73fe872bd06be6e9da9b8305
dabojimenez <dabojimenez16@hotmail.com>
2026-03-12 15:26:19 -0500

    Actores de la película

--- cinemapedia/lib/domain/datasource/movies_datasource.dart
+++ cinemapedia/lib/domain/datasource/movies_datasource.dart
@@ -3,8 +3,12 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 abstract class MoviesDatasource {
   // Definimos como luce los origenes de datos
   Future<List<Movie>> getNowPlaying({int page = 1});
+
   Future<List<Movie>> getPopular({int page = 1});
+
   Future<List<Movie>> getTopRated({int page = 1});
+
   Future<List<Movie>> getUpComming({int page = 1});
+
   Future<Movie> getMovieById(String id);
 }
