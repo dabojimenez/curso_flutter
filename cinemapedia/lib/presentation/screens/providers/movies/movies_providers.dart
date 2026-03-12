50b404f547dbc6ba2e67b9ce6c224919d032c788
dabojimenez <dabojimenez16@hotmail.com>
2026-03-11 19:14:04 -0500

    Evitar peticiones simultáneas

--- cinemapedia/lib/presentation/screens/providers/movies/movies_providers.dart
+++ cinemapedia/lib/presentation/screens/providers/movies/movies_providers.dart
@@ -2,29 +2,34 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/screens/providers/movies/movies_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final nowPlayingMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
-  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
-  return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
-});
+final nowPlayingMoviesProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+      final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+      return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
+    });
 
 // definimos el caso de uso, para cargar mas peliculas
 typedef MovieCallback = Future<List<Movie>> Function({int page});
 
 class MoviesNotifier extends StateNotifier<List<Movie>> {
-
   int currentPage = 0;
+  bool isLoading = false;
   MovieCallback fetchMoreMovies;
 
   // El estado inicial es una lista vacia
-  MoviesNotifier({
-    required this.fetchMoreMovies,
-  }) : super([]);
+  MoviesNotifier({required this.fetchMoreMovies}) : super([]);
 
   Future<void> loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+
     currentPage++;
     // Obtenemos las peliculas de la pagina actual
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     // Añadimos las peliculas al estado y regresamos un nuevo estado
     state = [...state, ...movies];
+    await Future.delayed(const Duration(milliseconds: 300));
+
+    isLoading = false;
   }
 }
