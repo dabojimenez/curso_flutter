97aae9013f228096c6b89d8fded24acc569ed376
dabojimenez <dabojimenez16@hotmail.com>
2026-03-12 14:13:56 -0500

    Realizar la petición HTTP y probar caché

--- cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -1,29 +1,41 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/screens/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatefulWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   static const String routeName = 'movie_screen';
   final String movieId;
 
   const MovieScreen({super.key, required this.movieId});
 
   @override
-  State<MovieScreen> createState() => _MovieScreenState();
+  MovieScreenState createState() => MovieScreenState();
 }
 
-class _MovieScreenState extends State<MovieScreen> {
+class MovieScreenState extends ConsumerState<MovieScreen> {
   @override
   void initState() {
     super.initState();
+    // Aqui mandamos a ejecutar la implemnebtacion http
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+
+    if (movie == null) {
+      return Scaffold(
+        body: const Center(child: CircularProgressIndicator(strokeWidth: 2)),
+      );
+    }
+
     return Scaffold(appBar: AppBar(title: Text('MovieId: ${widget.movieId}')));
   }
 }
