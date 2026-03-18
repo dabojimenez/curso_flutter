0749a059974e8ad3d961fc5a53d7a3d1820cbf95
dabojimenez <dabojimenez16@hotmail.com>
2026-03-17 20:11:00 -0500

    Creación de Vistas

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,7 +1,7 @@
-import 'package:cinemapedia/presentation/screens/providers/providers.dart';
-import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../views/views.dart';
+import '../../widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home_screen';
@@ -11,111 +11,8 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: _HomeView()),
+      body: Center(child: FavoriteView()),
       bottomNavigationBar: CustomButtomNavigationbar(),
-    );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-    ref.read(upComingMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final bool initialLoading = ref.watch(initialLoadingProvider);
-    if (initialLoading) return const FullScreenLoader();
-
-    // // aqui si es watch, por q necesitamso estar pendiente del estado
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    final upComingMovies = ref.watch(upComingMoviesProvider);
-    final moviesSlideShow = ref.watch(moviesSlideshowProvider);
-
-    return CustomScrollView(
-      // slivers: es un widget que trabaj directamente con el scrollview
-      slivers: [
-        // SliverAppBar: similar a un appbar tradicional, pero este funciona ocn un con el scroll
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            centerTitle: true,
-            title: CustomAppbar(),
-          ),
-        ),
-
-        SliverList(
-          // delegate: funcion que nos ayuda a crear los widgets
-          delegate: SliverChildBuilderDelegate((context, index) {
-            return Column(
-              children: [
-                MoviesSlideshow(movies: moviesSlideShow),
-                // MoviesSlideshow(movies: moviesSlideShow),
-                // Expanded(
-                //   child: ListView.builder(
-                //     itemCount: nowPlayingMovies.length,
-                //     itemBuilder: (context, index) {
-                //       final movie = nowPlayingMovies[index];
-                //       return ListTile(title: Text(movie.title));
-                //     },
-                //   ),
-                // ),
-                MovieHorizontalListview(
-                  movies: nowPlayingMovies,
-                  title: 'En Cines',
-                  subtitle: 'Lunes',
-                  loadNextPage: () {
-                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-
-                MovieHorizontalListview(
-                  movies: upComingMovies,
-                  title: 'Proximamente',
-                  subtitle: 'En este mes',
-                  loadNextPage: () {
-                    ref.read(upComingMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-
-                MovieHorizontalListview(
-                  movies: popularMovies,
-                  title: 'Populares',
-                  // subtitle: 'Lunes',
-                  loadNextPage: () {
-                    ref.read(popularMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-
-                MovieHorizontalListview(
-                  movies: topRatedMovies,
-                  title: 'Mejor Calificadas',
-                  subtitle: 'Todos los tiempos',
-                  loadNextPage: () {
-                    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-
-                const SizedBox(height: 25),
-              ],
-            );
-          }, childCount: 1),
-        ),
-      ],
     );
   }
 }
