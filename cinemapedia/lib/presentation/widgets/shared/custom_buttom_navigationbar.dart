e65d2d07e87bdf1f332d8c0283e44df23100a404
dabojimenez <dabojimenez16@hotmail.com>
2026-03-17 19:58:00 -0500

    Bottom Navigation Bar - Navegación

--- cinemapedia/lib/presentation/widgets/shared/custom_buttom_navigationbar.dart
+++ cinemapedia/lib/presentation/widgets/shared/custom_buttom_navigationbar.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomButtomNavigationbar extends StatelessWidget {
   const CustomButtomNavigationbar({super.key});
+
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).fullPath ?? '/';
+
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        // context.go('/categories');
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: getCurrentIndex(context),
+      onTap: (value) {
+        // value: es el indice de la posicion o el tab seleccionado
+        // 0 => Home
+        // 1 => Categories
+        // 2 => Favorites
+        onItemTapped(context, value);
+      },
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
         BottomNavigationBarItem(
