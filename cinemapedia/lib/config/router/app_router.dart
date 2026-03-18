4fa78bfbe1c49c01cd45d6bd1ed0b3793725f56f
dabojimenez <dabojimenez16@hotmail.com>
2026-03-17 19:48:43 -0500

    ShellRoute - GoRouter

--- cinemapedia/lib/config/router/app_router.dart
+++ cinemapedia/lib/config/router/app_router.dart
@@ -5,23 +5,57 @@ import 'package:go_router/go_router.dart';
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      name: 'home',
-      builder: (context, state) => const HomeScreen(childView: HomeView()),
-      // routes: son las rutas hijas
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      // definimos las rutas de tipo gouroute
       routes: [
         GoRoute(
-          // :id => indicamos que le pasamos un argumento, en este caos el id
-          path: 'movie/:id',
-          name: MovieScreen.routeName,
+          path: '/',
           builder: (context, state) {
-            // Obtenemos el id de la ruta
-            final movieId = state.pathParameters['id'] ?? 'no-id';
-            return MovieScreen(movieId: movieId);
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              // :id => indicamos que le pasamos un argumento, en este caos el id
+              path: 'movie/:id',
+              name: MovieScreen.routeName,
+              builder: (context, state) {
+                // Obtenemos el id de la ruta
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              },
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
           },
         ),
       ],
     ),
+
+    //// Rutas padre / hijo
+    // GoRoute(
+    //   path: '/',
+    //   name: 'home',
+    //   builder: (context, state) => const HomeScreen(childView: HomeView()),
+    //   // routes: son las rutas hijas
+    //   routes: [
+    //     GoRoute(
+    //       // :id => indicamos que le pasamos un argumento, en este caos el id
+    //       path: 'movie/:id',
+    //       name: MovieScreen.routeName,
+    //       builder: (context, state) {
+    //         // Obtenemos el id de la ruta
+    //         final movieId = state.pathParameters['id'] ?? 'no-id';
+    //         return MovieScreen(movieId: movieId);
+    //       },
+    //     ),
+    //   ],
+    // ),
   ],
 );
