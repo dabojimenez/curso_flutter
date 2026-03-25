688233b5dfa9c1ccc7cdd3223ec120c8ecb7bbc6
dabojimenez <dabojimenez16@hotmail.com>
2026-03-25 15:42:08 -0500

    Estructura inicial de la aplicación

---
+++ forms_app/lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bloc counter'),
+        actions: [
+          IconButton(onPressed: () => {}, icon: Icon(Icons.refresh_rounded)),
+        ],
+      ),
+      body: const Center(child: Text('counter value: xxxx')),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            child: const Text('+3'),
+            onPressed: () {},
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '2',
+            child: const Text('+2'),
+            onPressed: () {},
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '3',
+            child: const Text('+1'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
