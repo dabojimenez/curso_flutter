ade0a9ed31599032e3e62938dcd3e5aa061646a0
dabojimenez <dabojimenez16@hotmail.com>
2026-03-27 14:28:51 -0500

    Consideraciones con inputs y scroll

--- forms_app/lib/presentation/screens/register_screen.dart
+++ forms_app/lib/presentation/screens/register_screen.dart
@@ -17,6 +17,34 @@ class _RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    // SafeArea: recomendado usar para evitar que el contenido se superponga con las áreas especiales de la pantalla (como las barras de estado)
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        // SingleChildScrollView: permite que el contenido sea desplazable si no cabe en la pantalla, util para cuando se usan widgets muy grandes o columnas
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const FlutterLogo(size: 100),
+
+              TextFormField(),
+              TextFormField(),
+              TextFormField(),
+              TextFormField(),
+              TextFormField(),
+
+              const SizedBox(height: 20),
+              FilledButton.tonalIcon(
+                onPressed: () {},
+                icon: const Icon(Icons.save),
+                label: const Text('Crear usuario'),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
