d41b72a4dbf2fa7e826676f041c1c0b955069a1e
dabojimenez <dabojimenez16@hotmail.com>
2026-03-27 21:04:33 -0500

    Formulario tradicional

--- forms_app/lib/presentation/screens/register_screen.dart
+++ forms_app/lib/presentation/screens/register_screen.dart
@@ -41,22 +41,67 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? username = '';
+  String? email = '';
+  String? password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
         children: [
-          CustomTextFormFiel(label: 'Nombre usuario'),
+          CustomTextFormFiel(
+            label: 'Nombre usuario',
+            onChanged: (value) => username = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo requerido';
+              if (value.trim().isEmpty) return 'Campo requerido';
+              if (value.length < 6) return 'Más de 6 letras';
+              return null;
+            },
+          ),
           const SizedBox(height: 20),
-          CustomTextFormFiel(label: 'Correo electrónico'),
+          CustomTextFormFiel(
+            label: 'Correo electrónico',
+            onChanged: (value) => email = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo requerido';
+              if (value.trim().isEmpty) return 'Campo requerido';
+              final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+              if (!emailRegExp.hasMatch(value))
+                return 'Correo no tiene formato de correo';
+              return null;
+            },
+          ),
           const SizedBox(height: 20),
-          CustomTextFormFiel(label: 'Contraseña', obscureText: true),
+          CustomTextFormFiel(
+            label: 'Contraseña',
+            obscureText: true,
+            onChanged: (value) => password = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo requerido';
+              if (value.trim().isEmpty) return 'Campo requerido';
+              if (value.length < 6) return 'Más de 6 letras';
+              return null;
+            },
+          ),
           const SizedBox(height: 20),
           FilledButton.tonalIcon(
-            onPressed: () {},
+            onPressed: () {
+              final isValid = _formKey.currentState!.validate();
+              if (!isValid) return;
+              print('$username, $email, $password');
+            },
             icon: const Icon(Icons.save),
             label: const Text('Crear usuario'),
           ),
