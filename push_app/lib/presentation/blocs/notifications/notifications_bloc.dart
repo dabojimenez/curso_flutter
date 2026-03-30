244cc892e6e2ac754c8d976db0adcb05a09ef359
dabojimenez <dabojimenez16@hotmail.com>
2026-03-30 12:24:19 -0500

    Solicitar permisos

--- push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
+++ push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -6,9 +6,26 @@ part 'notifications_event.dart';
 part 'notifications_state.dart';
 
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
   NotificationsBloc() : super(const NotificationsState()) {
     // on<NotificationsEvent>((event, emit) {
     //   // TODO: implement event handler
     // });
+  }
+
+  // Metodo que nos permitira obtener el permiso
+  void requestPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true, // crea una medalliitra en la aplicacion
+      carPlay: false, // si el dispositivo esta conectado a un auto
+      criticalAlert: false, // notificaciones criticas o  si son muy urgentes
+      provisional: false, // permiso provisional
+      sound: true,
+    );
+
+    print('Permiso concedido: ${settings.authorizationStatus}');
   }
 }
