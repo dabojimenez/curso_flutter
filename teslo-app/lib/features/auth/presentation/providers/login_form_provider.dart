183e2e860357cec117b7f9b0f08b5f49bf493b00
dabojimenez <dabojimenez16@hotmail.com>
2026-04-05 20:57:20 -0500

    LoginForm Provider y Notifier

--- teslo-app/lib/features/auth/presentation/providers/login_form_provider.dart
+++ teslo-app/lib/features/auth/presentation/providers/login_form_provider.dart
@@ -1,4 +1,6 @@
 //! 1 - State del provider
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:formz/formz.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
 class LoginFormsState {
@@ -14,7 +16,79 @@ class LoginFormsState {
       this.isValid = false,
       this.email = const Email.pure(),
       this.password = const Password.pure()});
+
+  LoginFormsState copyWith({
+    bool? isPosting,
+    bool? ifFormPosted,
+    bool? isValid,
+    Email? email,
+    Password? password,
+  }) {
+    return LoginFormsState(
+      isPosting: isPosting ?? this.isPosting,
+      ifFormPosted: ifFormPosted ?? this.ifFormPosted,
+      isValid: isValid ?? this.isValid,
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
+
+  @override
+  String toString() => '''
+  isPosting: $isPosting
+  ifFormPosted: $ifFormPosted
+  isValid: $isValid
+  email: $email
+  password: $password
+''';
 }
+
 //! 2 - Como implementamos un notifier
+class LoginFormNotifier extends StateNotifier<LoginFormsState> {
+  LoginFormNotifier() : super(LoginFormsState());
+
+  onEmailChange(String value) {
+    final email = Email.dirty(value);
+    state = state.copyWith(
+      email: email,
+      isValid: Formz.validate([email, state.password]),
+    );
+  }
+
+  onPasswordChange(String value) {
+    final password = Password.dirty(value);
+    state = state.copyWith(
+      password: password,
+      isValid: Formz.validate([state.email, password]),
+    );
+  }
+
+  onFormSubmit() {
+    _touchEveryField();
+
+    // Si el formulario no es válido, no se hace nada
+    if (!state.isValid) return;
+
+    print(state);
+  }
+
+  // Tocamos todos los campos del formulario, para aplicar las validaciones y mostrar los errores
+  _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+
+    state = state.copyWith(
+      email: email,
+      password: password,
+      isValid: Formz.validate([email, password]),
+      ifFormPosted: true,
+    );
+  }
+}
 
 //! 3 - StateNotifierprovider - consume afuera
+final LoginFormProvider =
+    StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormsState>(
+        (ref) {
+  return LoginFormNotifier();
+});
