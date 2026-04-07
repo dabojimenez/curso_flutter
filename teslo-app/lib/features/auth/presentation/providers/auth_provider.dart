717d7c800af2b642df45cdb46f00ddfe318d788d
dabojimenez <dabojimenez16@hotmail.com>
2026-04-07 17:53:04 -0500

    Auth Provider

---
+++ teslo-app/lib/features/auth/presentation/providers/auth_provider.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:teslo_shop/features/auth/domain/domain.dart';
+
+import '../../infrastructure/repositories/auth_repository_impl.dart';
+
+final authProvider =
+    StateNotifierProvider.autoDispose<AuthNotifier, AuthState>((ref) {
+  final authRepository = AuthRepositoryImpl();
+  return AuthNotifier(authRepository: authRepository);
+});
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  final AuthRepository authRepository;
+  AuthNotifier({required this.authRepository}) : super(AuthState());
+
+  void loginuser(String email, String password) async {
+    // await authRepository.login(email, password);
+    // state =
+  }
+  void registerUser(String email, String password, String fullName) async {}
+  void checkAuthStatus() async {}
+}
+
+enum AuthStatus {
+  checking,
+  authenticated,
+  notAuthenticated,
+}
+
+class AuthState {
+  final AuthStatus authStatus;
+  final User? user;
+  final String errorMessage;
+
+  AuthState({
+    this.authStatus = AuthStatus.checking,
+    this.user,
+    this.errorMessage = '',
+  });
+
+  AuthState copyWith({
+    AuthStatus? authStatus,
+    User? user,
+    String? errorMessage,
+  }) =>
+      AuthState(
+        authStatus: authStatus ?? this.authStatus,
+        user: user ?? this.user,
+        errorMessage: errorMessage ?? this.errorMessage,
+      );
+}
