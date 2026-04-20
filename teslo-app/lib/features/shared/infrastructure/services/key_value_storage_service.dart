239fc853c3b86d90a8de9c659eef4a490f6b4bc7
dabojimenez <dabojimenez16@hotmail.com>
2026-04-20 10:16:25 -0500

    Implementar el patrón adaptador

--- teslo-app/lib/features/shared/infrastructure/services/key_value_storage_service.dart
+++ teslo-app/lib/features/shared/infrastructure/services/key_value_storage_service.dart
@@ -1,5 +1,5 @@
 abstract class KeyValueStorageService {
-  Future<void> setKeyValue(String key, value);
-  Future getValue(String key, value);
+  Future<void> setKeyValue<T>(String key, T value);
+  Future<T?> getValue<T>(String key);
   Future<bool> removeKey(String key);
 }
